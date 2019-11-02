dcfe4c5ee98fc2ad376e12e0d9fff7f5d02497c3
hongzhi pan <victoryhongzhi@gmail.com>
2019-11-02 17:46:27 -0400

    make material-ui button work

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
       <header className="App-header">
@@ -19,7 +31,36 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button variant="contained" className={classes.button}>
+        Default
+      </Button>
+      <Button variant="contained" color="primary" className={classes.button}>
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary" className={classes.button}>
+        Secondary
+      </Button>
+      <Button variant="contained" color="secondary" disabled className={classes.button}>
+        Disabled
+      </Button>
+      <Button variant="contained" href="#contained-buttons" className={classes.button}>
+        Link
+      </Button>
+      <input
+        accept="image/*"
+        className={classes.input}
+        id="contained-button-file"
+        multiple
+        type="file"
+      />
+      <label htmlFor="contained-button-file">
+        <Button variant="contained" component="span" className={classes.button}>
+          Upload
+        </Button>
+      </label>
     </div>
+
+    
   );
 }
 
