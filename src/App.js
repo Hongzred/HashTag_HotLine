7791160bbb25273bc199f43a6045226f9c5e0d78
hongzhi pan <victoryhongzhi@gmail.com>
2019-11-02 23:30:33 -0400

    import dashboard interface to the app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Dashboard from './dashboard';
 
 const useStyles = makeStyles(theme => ({
   button: {
@@ -17,20 +18,7 @@ function App() {
   const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Dashboard></Dashboard>
       <Button variant="contained" className={classes.button}>
         Default
       </Button>
