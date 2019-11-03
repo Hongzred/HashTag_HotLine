42d3ffb4452d9737d7d39ee1724bc530c839544f
hongzhi pan <victoryhongzhi@gmail.com>
2019-11-03 00:28:12 -0400

    remove test buttons

--- src/App.js
+++ src/App.js
@@ -19,33 +19,7 @@ function App() {
   return (
     <div className="App">
      <Dashboard></Dashboard>
-      <Button variant="contained" className={classes.button}>
-        Default
-      </Button>
-      <Button variant="contained" color="primary" className={classes.button}>
-        Primary
-      </Button>
-      <Button variant="contained" color="secondary" className={classes.button}>
-        Secondary
-      </Button>
-      <Button variant="contained" color="secondary" disabled className={classes.button}>
-        Disabled
-      </Button>
-      <Button variant="contained" href="#contained-buttons" className={classes.button}>
-        Link
-      </Button>
-      <input
-        accept="image/*"
-        className={classes.input}
-        id="contained-button-file"
-        multiple
-        type="file"
-      />
-      <label htmlFor="contained-button-file">
-        <Button variant="contained" component="span" className={classes.button}>
-          Upload
-        </Button>
-      </label>
+      
     </div>
 
     
