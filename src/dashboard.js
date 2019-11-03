cf0ae7dd25915bbd1d3ecdb2d82763a703ed8d7a
hongzhi pan <victoryhongzhi@gmail.com>
2019-11-03 00:20:57 -0400

    update dashboard according to our design

--- src/dashboard.js
+++ src/dashboard.js
@@ -16,11 +16,9 @@ import Paper from '@material-ui/core/Paper';
 import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-// import NotificationsIcon from '@material-ui/icons/Notifications';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import { mainListItems, secondaryListItems } from './listItems';
-// import Chart from './Chart';
-// import Deposits from './Deposits';
-// import Orders from './Orders';
+
 
 function Copyright() {
   return (
@@ -145,8 +143,8 @@ export default function Dashboard() {
             Dashboard
           </Typography>
           <IconButton color="inherit">
-            <Badge badgeContent={4} color="secondary">
-              {/* <NotificationsIcon /> */}
+            <Badge badgeContent={0} color="secondary">
+              <AccountCircleIcon />
             </Badge>
           </IconButton>
         </Toolbar>
@@ -172,10 +170,10 @@ export default function Dashboard() {
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
           <Grid container spacing={3}>
-            {/* Chart */}
+
             <Grid item xs={12} md={8} lg={9}>
               <Paper className={fixedHeightPaper}>
-                {/* <Chart /> */}
+
               </Paper>
             </Grid>
             {/* Recent Deposits */}
@@ -184,10 +182,10 @@ export default function Dashboard() {
                 {/* <Deposits /> */}
               </Paper>
             </Grid>
-            {/* Recent Orders */}
+
             <Grid item xs={12}>
               <Paper className={classes.paper}>
-                {/* <Orders /> */}
+
               </Paper>
             </Grid>
           </Grid>
