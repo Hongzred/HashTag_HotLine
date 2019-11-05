a540f1dd911f8ae0d14d810aefcc8e28bff47ae5
David Hadaller <dahadaller@gmail.com>
2019-11-05 00:36:00 -0500

    added small twitter feed file reminder (non-functional template)

---
+++ src/components/TwitterFeed/TwitterFeed.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// this stateless function will render the 
+// streaming twitter data in the app's react hook
+function TwitterFeed() {
+    return (
+        //Place Material UI JSX here
+    );
+}
+
+export default TwitterFeed;
