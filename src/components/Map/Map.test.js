71eb101eb7b81b86a8706df76ca62d224e3b4b17
Samuel Fils <smfils1@gmail.com>
2019-11-05 20:03:42 -0500

    Added fake data and marker tests

--- src/components/Map/Map.test.js
+++ src/components/Map/Map.test.js
@@ -11,17 +11,46 @@ const props = {
     zoom: 10
 };
 
+const issues = [
+    { latitude: 40.73061, longitude: -73.935242, description: "description1", id:"123" },
+    { latitude: 40.74161, longitude: -73.946242, description: "description2", id:"321" }
+];
+
 jest.mock("../../__mocks__/react-map-gl");
 afterEach(cleanup);
 
 describe("Map Component", () => {
     it("renders the map", () => {
-        const { getByTestId, debug } = render(<Map {...props} />);
+        const { getByTestId } = render(<Map {...props} />);
         const map = getByTestId("mapbox");
         expect(map).toBeInTheDocument();
         expect(map).toHaveStyle(`
 		width: 400px;
 		height: 400px;
-	  `);
+      `);
+    });
+
+    it("renders the map with a marker", () => {
+        const issue = issues[0];
+        const { getAllByTestId } = render(
+            <Map {...props} issues={[issue]} />
+        );
+        const markers = getAllByTestId('marker');
+        expect(markers[0]).toHaveTextContent(`${issue.latitude}:${issue.longitude}`)
+        expect(markers.length).toBe(1)
+
+    });
+
+    it("renders the map with multiple markers", () => {
+        const [issue1, issue2] = issues
+        const { getAllByTestId, getByText } = render(
+            <Map {...props} issues={issues} />
+        );
+        const markers = getAllByTestId(`marker`);
+        getByText(`${issue1.latitude}:${issue1.longitude}`)
+        getByText(`${issue2.latitude}:${issue2.longitude}`)
+        getByText(`description1`)
+        getByText(`description2`)
+        expect(markers.length).toBe(2)
     });
 });
