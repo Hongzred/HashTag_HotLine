16727b8cb5066bf7fb0abd5db20128382c14bda6
Samuel Fils <smfils1@gmail.com>
2019-11-06 00:38:57 -0500

    Added pin component w/ test

---
+++ src/components/Pin/Pin.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Pin from "./Pin";
+
+
+const props = { pinClickHandler:jest.fn() };
+
+jest.mock("../../__mocks__/@material-ui/icons");
+afterEach(cleanup);
+
+describe("Pin Component", () => {
+    it("renders a pin", () => {
+        const { getByText } = render(<Pin/>);
+        const icon = getByText("ICON");
+    });
+
+    it("renders a pin click", () => {
+        const { getByTestId } = render(<Pin {...props} />);
+        const pin = getByTestId("pin");
+        fireEvent.click(pin)
+        expect(props.pinClickHandler).toHaveBeenCalledTimes(1);
+    });
+    
+});
