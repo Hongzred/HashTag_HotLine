cf9c2f8a95d07963ad8aaf055cfcdca42a0638ee
hongzhi pan <victoryhongzhi@gmail.com>
2019-11-07 23:44:30 -0500

    generate sample queries for tweet

---
+++ src/graphql/mutations.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createTweet = `mutation CreateTweet($input: CreateTweetInput!) {
+  createTweet(input: $input) {
+    id
+    name
+    description
+    latitude
+    lontitude
+  }
+}
+`;
+export const updateTweet = `mutation UpdateTweet($input: UpdateTweetInput!) {
+  updateTweet(input: $input) {
+    id
+    name
+    description
+    latitude
+    lontitude
+  }
+}
+`;
+export const deleteTweet = `mutation DeleteTweet($input: DeleteTweetInput!) {
+  deleteTweet(input: $input) {
+    id
+    name
+    description
+    latitude
+    lontitude
+  }
+}
+`;
