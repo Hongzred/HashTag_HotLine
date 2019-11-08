a3248eda86377e91161b8a9f2ece27f992daa562
hongzhi pan <victoryhongzhi@gmail.com>
2019-11-07 23:23:37 -0500

    integrate auth to our app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,10 @@ import "./App.css";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Dashboard from "../Dashboard/Dashboard";
+import Amplify from "aws-amplify";
+import awsconfig from "../../aws-exports";
+import { withAuthenticator } from 'aws-amplify-react';
+Amplify.configure(awsconfig);
 
 const useStyles = makeStyles(theme => ({
     button: {
@@ -23,4 +27,4 @@ function App() {
     );
 }
 
-export default App;
+export default withAuthenticator(App, true);
