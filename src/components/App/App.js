94706650a6e3c4b3318910f73f30be0303bf9633
hongzhi pan <victoryhongzhi@gmail.com>
2019-11-15 23:13:48 -0500

    fixed authentication error, make it working

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,9 +5,9 @@ import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Dashboard from "../Dashboard/Dashboard";
 import Amplify from "aws-amplify";
-//import awsconfig from "../../aws-exports";
+import awsconfig from "../../aws-exports";
 import { withAuthenticator } from 'aws-amplify-react';
-//Amplify.configure(awsconfig);
+Amplify.configure(awsconfig);
 
 const useStyles = makeStyles(theme => ({
     button: {
