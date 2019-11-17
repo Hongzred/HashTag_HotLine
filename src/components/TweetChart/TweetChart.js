902714bb71e63a5e63de6d870744779d686c1274
David Hadaller <dahadaller@gmail.com>
2019-11-16 23:03:41 -0500

    Added data generator to chart

--- src/components/TweetChart/TweetChart.js
+++ src/components/TweetChart/TweetChart.js
@@ -1,23 +1,49 @@
-import React from 'react';
-import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
-import PropTypes from 'prop-types';
-import Typography from '@material-ui/core/Typography';
+// import React from 'react';
+// import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
+// import PropTypes from 'prop-types';
+// import Typography from '@material-ui/core/Typography';
 
-function Title(props) {
-  return (
-    <Typography
-    	component="h2" 
-    	variant="h6" 
-    	color="primary" 
-    	gutterBottom>
-    	
-      	{props.children}
-    </Typography>
-  );
+// Generate Sales Data
+function createData() {
+	let data = [];
+	let hour = '';
+	let tweets = 0;
+	for (let i = 0; i < 24; i+=3) {
+		tweets = Math.floor(Math.random() * 30)
+		if (i<10){
+			hour = '0' + i.toString() + ':00'
+		}
+		else {
+			hour = i.toString() + ':00'
+		}
+		
+		data.push(
+			[hour, tweets]
+		)
+	}
+	data.push(['24:00', undefined]);
+	return data;
 }
 
-Title.propTypes = {
-  children: PropTypes.node,
-};
+const data = createData();
+console.log(data);
+
+// // Title Heading for Chart
+// function Title(props) {
+//   return (
+//     <Typography
+//     	component="h2" 
+//     	variant="h6" 
+//     	color="primary" 
+//     	gutterBottom>
+    	
+//       	{props.children}
+//     </Typography>
+//   );
+// }
+
+// Title.propTypes = {
+//   children: PropTypes.node,
+// };
 
 // TODO: Implement Chart function
