96c56d6bac4bc41543ce9c46f2a8ebba82f5e49c
David Hadaller <dahadaller@gmail.com>
2019-11-16 23:12:34 -0500

    added stateless TweetChart functional component

--- src/components/TweetChart/TweetChart.js
+++ src/components/TweetChart/TweetChart.js
@@ -1,9 +1,10 @@
-// import React from 'react';
-// import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
-// import PropTypes from 'prop-types';
-// import Typography from '@material-ui/core/Typography';
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
 
-// Generate Sales Data
+// Generate Sales Data 
+// TODO: DELETE createData() and get new data with graphql
 function createData() {
 	let data = [];
 	let hour = '';
@@ -24,26 +25,52 @@ function createData() {
 	data.push(['24:00', undefined]);
 	return data;
 }
+const data = createData(); 
 
-const data = createData();
-console.log(data);
-
-// // Title Heading for Chart
-// function Title(props) {
-//   return (
-//     <Typography
-//     	component="h2" 
-//     	variant="h6" 
-//     	color="primary" 
-//     	gutterBottom>
+// Title Heading for Chart
+function Title(props) {
+  return (
+    <Typography
+    	component="h2" 
+    	variant="h6" 
+    	color="primary" 
+    	gutterBottom>
     	
-//       	{props.children}
-//     </Typography>
-//   );
-// }
+      	{props.children}
+    </Typography>
+  );
+}
 
-// Title.propTypes = {
-//   children: PropTypes.node,
-// };
+Title.propTypes = {
+  children: PropTypes.node,
+};
 
-// TODO: Implement Chart function
+// TweetChart component
+export default function TweetChart() {
+	return (
+	  <React.Fragment> 
+		{/*need React.Fragments because two components are returned here: 
+		the title and responsive container that contains TweetChart*/}
+		<Title>Tweets over Time</Title>
+		<ResponsiveContainer>
+		  <LineChart
+			data={data}
+			margin={{
+			  top: 16,
+			  right: 16,
+			  bottom: 0,
+			  left: 24,
+			}}
+		  >
+			<XAxis dataKey="time" />
+			<YAxis>
+			  <Label angle={270} position="left" style={{ textAnchor: 'middle' }}>
+				Sales ($)
+			  </Label>
+			</YAxis>
+			<Line type="monotone" dataKey="amount" stroke="#556CD6" dot={false} />
+		  </LineChart>
+		</ResponsiveContainer>
+	  </React.Fragment>
+	);
+  }
