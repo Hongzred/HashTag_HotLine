b4397e0e13b765ff235425c66c69eb38f531001c
Samuel Fils <smfils1@gmail.com>
2019-11-22 23:32:27 -0500

    reconfigured routing that is more convenient

---
+++ src/components/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Layout
+import Layout from "./Layout/Layout";
+
+// pages
+import Dashboard from "../pages/Dashboard/Dashboard";
+import Analytics from "../pages/Analytics/Analytics";
+import Feed from "../pages/Feed/Feed";
+import Settings from "../pages/Settings/Settings";
+import Error from "../pages/Error/Error";
+
+//aws
+import Amplify from "aws-amplify";
+import awsconfig from "../aws-exports";
+import { withAuthenticator } from 'aws-amplify-react';
+Amplify.configure(awsconfig);
+
+const routes = [
+  { path: "/", title:"Overview", page: Dashboard },
+  { path: "/analytics", title:"Analytics", page: Analytics },
+  { path: "/feed", title:"Feed", page: Feed },
+  { path: "/settings", title:"Settings", page: Settings },
+];
+
+const App = () => {
+  const navigation = routes.map(({path,title}) => (
+    { 
+      link:path,
+      label:title
+    }
+  ))
+  return (
+    <Router>
+      <Switch>
+        {routes.map(({ path, page, title }, index) => (
+          <Route key={index} exact path={path} component={() => <Layout page={page} title={title} navigation={navigation}/>} />
+        ))}
+        <Route component={Error} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
-import * as serviceWorker from './serviceWorker';
-require('dotenv').config() // Need DotEnv for the API Tokens. If you need the .env file, it should be pinned on slack. 
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "@material-ui/styles";
+import { CssBaseline } from "@material-ui/core";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Themes from "./themes";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+import { LayoutProvider } from "./context/LayoutContext";
+
+ReactDOM.render(
+  <LayoutProvider>
+      <ThemeProvider theme={Themes.default}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+  </LayoutProvider>,
+  document.getElementById("root"),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
