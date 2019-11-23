afe38457703510920c3e7dcb2a79849da6ee6d84
Samuel Fils <smfils1@gmail.com>
2019-11-22 22:55:57 -0500

    Added sidebarlink styling

---
+++ src/components/SidebarLink/styles.js
@@ -0,0 +1,36 @@
+import { makeStyles } from "@material-ui/styles";
+
+export default makeStyles(theme => ({
+  link: {
+    textDecoration: "none",
+    "&:hover, &:focus": {
+      backgroundColor: theme.palette.background.light,
+    },
+  },
+  linkActive: {
+    backgroundColor: theme.palette.background.light,
+  },
+  linkIcon: {
+    marginRight: theme.spacing(1),
+    color: theme.palette.text.secondary + "99",
+    transition: theme.transitions.create("color"),
+    width: 24,
+    display: "flex",
+    justifyContent: "center",
+  },
+  linkIconActive: {
+    color: theme.palette.primary.main,
+  },
+  linkText: {
+    padding: 0,
+    color: theme.palette.text.secondary + "CC",
+    transition: theme.transitions.create(["opacity", "color"]),
+    fontSize: 16,
+  },
+  linkTextActive: {
+    color: theme.palette.text.primary,
+  },
+  linkTextHidden: {
+    opacity: 0,
+  }
+}));
