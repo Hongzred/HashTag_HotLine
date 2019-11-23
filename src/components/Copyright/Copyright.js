d5acde2c9999bb05862d085e3c91bfe6539197f2
Samuel Fils <smfils1@gmail.com>
2019-11-22 22:57:24 -0500

    Added seperate copyright component

---
+++ src/components/Copyright/Copyright.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link, Typography } from '@material-ui/core';
+
+export default function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {"Copyright © "}
+            <Link color="inherit" href="#">
+                Hashtag Hotline
+            </Link>{" "}
+            {new Date().getFullYear()}
+            {"."}
+        </Typography>
+    );
+}
