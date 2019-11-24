66ebcd8071562831f18f827a3540f8556d1f0035
Samuel Fils <smfils1@gmail.com>
2019-11-24 12:30:47 -0500

    Added input change handlers & onSave handler that logs the data to submits for settings form

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,11 +1,41 @@
-import React from "react";
+
+
+import React, {Component} from "react";
 import { FormControl, InputLabel, Input, Button } from "@material-ui/core";
 import ChipInput from "material-ui-chip-input";
 
-const dumbyHashtags = ["#abc", "#cba", "#qwe"];
-export default function Settings(props) {
-    return (
-        <>
+const dummySettings = {
+    botMessage: "Your report is in review",
+    hashtags:["#abc", "#cba", "#qwe"]
+};
+export default class Settings extends Component {
+    state = {
+        botMessage:"",
+        hashtags:[]
+    }
+
+    componentDidMount(){
+        this.setState(dummySettings)
+    }
+    onMessageChange = (e) => {
+        this.setState({
+            botMessage:e.target.value
+        })
+    }
+
+    onHashtagsChange = (e) => {
+        this.setState({
+            hashtags:e
+        })        
+    }
+
+    onSave = (e) => {
+        e.preventDefault()
+        console.log("Submitted",this.state)
+    }
+
+    render(){
+        return (
             <div
                 style={{
                     display: "flex",
@@ -14,24 +44,26 @@ export default function Settings(props) {
                     padding: 20
                 }}
             >
-                <form style={{ width: "80%" }}>
+                <form style={{ width: "80%" }} onSubmit={this.onSave}>
                     <ChipInput
-                        defaultValue={dumbyHashtags}
+                        defaultValue={this.state.hashtags}
                         fullWidth
                         label="Hashtags"
                         placeholder="Type and press enter to add hashtags"
+                        onChange={this.onHashtagsChange}
                     />
                     <FormControl margin="normal" fullWidth>
                         <InputLabel htmlFor="botMessage">
                             Bot Message
                         </InputLabel>
-                        <Input id="botMessage" type="text" />
+                        <Input id="botMessage" type="text" onChange={this.onMessageChange} value={this.state.botMessage}/>
                     </FormControl>
-                    <Button variant="contained" color="primary" size="medium">
+                    <Button variant="contained" color="primary" size="medium" type="submit">
                         Save
                     </Button>
                 </form>
             </div>
-        </>
     );
+    } 
+    
 }
