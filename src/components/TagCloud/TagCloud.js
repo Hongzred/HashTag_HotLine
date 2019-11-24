1f7c53c5ce50dc9ed2abd4e9c0032b4ebb9433d2
David Hadaller <dahadaller@gmail.com>
2019-11-24 00:33:59 -0500

    Finished handleDelete handler for when tags are deleted

--- src/components/TagCloud/TagCloud.js
+++ src/components/TagCloud/TagCloud.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Chip from '@material-ui/core/Chip';
-import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,35 +18,45 @@ const useStyles = makeStyles(theme => ({
 export default function ChipsArray() {
   const classes = useStyles();
   const [chipData, setChipData] = React.useState([
-    { key: 0, active: true, label: '#gasleak' },
-    { key: 1, active: true, label: '@coned' },
-    { key: 2, active: true, label: '#help' }
+    { key: 0, disabled: false, color: "primary", label: '#gasleak' },
+    { key: 1, disabled: false, color: "primary", label: '@coned' },
+    { key: 2, disabled: false, color: "primary", label: '#help' }
   ]);
 
-  const handleDelete = chipToDelete => () => {
-    setChipData(chips => chips.map(chip => 
-    	{
-    		chip.active = false;
-    		return chip;
-    	}));
-  };
+    //this arrow function returns ANOTHER arrow function
+    //so the handler has to be passed an argument
+    // handleDelete(chip) gets you a function to delete the chip
+    // so it isn't executed immediately (as might be the case if
+    // the function returned a primitive like a number or object or something
+    const handleDelete = chipToDelete => () => {
+        setChipData(chips => chips.map(chip =>{
+                if(chip.key === chipToDelete.key){
+                    return {...chip, disabled:true, color:"secondary"}
+                }
+                else {
+                    return chip
+                }
+            }
 
-  return (
-    <div className={classes.root}>
-      {chipData.map(data => {
+        ));
+      };
 
-        return (
-          <Chip
-            key={data.key}
-            label={data.label}
-            onDelete={handleDelete(data)}
-            className={classes.chip}
-            color="primary"
-            disabled={data.active}
-          />
-        );
+    return (
+        <div className={classes.root}>
+          {chipData.map(chip => {
 
-      })}
-    </div>
-  );
+            return (
+              <Chip
+                key={chip.key}
+                label={chip.label}
+                onDelete={handleDelete(chip)}
+                className={classes.chip}
+                color={chip.color}
+                disabled={chip.disabled}
+              />
+            );
+
+          })}
+        </div>
+    );
 }
