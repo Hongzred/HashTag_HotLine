bbc509e317ccf19a974fd02378392d8e181560de
Samuel Fils <smfils1@gmail.com>
2019-11-25 15:43:45 -0500

    Added util function to get difference btw arrays & settings page with backend connection

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,40 +1,120 @@
-
-
-import React, {Component} from "react";
+import React, { Component } from "react";
+import { API, graphqlOperation } from "aws-amplify";
 import { FormControl, InputLabel, Input, Button } from "@material-ui/core";
 import ChipInput from "material-ui-chip-input";
+import { listSettings as allSettings } from "../../custom_graphql/queries";
+import { listHashtags } from "../../graphql/queries";
+import {
+    createSetting,
+    updateSetting,
+    createHashtag,
+    deleteHashtag
+} from "../../graphql/mutations";
+import symmetricDifference from "../../utils/symmetricDifference";
 
 const dummySettings = {
     botMessage: "Your report is in review",
-    hashtags:["#abc", "#cba", "#qwe"]
+    hashtags: ["#abc", "#cba", "#qwe"]
 };
 export default class Settings extends Component {
     state = {
-        botMessage:"",
-        hashtags:[]
+        botMessage: "",
+        hashtags: []
+    };
+    settingsId = null;
+
+    async componentDidMount() {
+        this.setState(dummySettings); //We need to initialize w/ real settings from DB
+        await this.createUserSettings();
     }
 
-    componentDidMount(){
-        this.setState(dummySettings)
+    async getUserSettings() {
+        const {
+            data: {
+                listSettings: { items }
+            }
+        } = await API.graphql(graphqlOperation(allSettings));
+        const settings = items[0];
+        if (items[0]) this.settingsId = settings.id;
+
+        return settings;
     }
-    onMessageChange = (e) => {
+
+    async getHashtagByName(hashtag) {
+        const {
+            data: {
+                listHashtags: { items }
+            }
+        } = await API.graphql(graphqlOperation(listHashtags));
+        const filteredHashtags = items.filter(({ name }) => name === hashtag);
+        if (filteredHashtags[0]) return filteredHashtags[0].id;
+    }
+    //We should create a lambda function instead to be trigger once the user signs in.
+    //This creates a settings entry in the databases for the user. User will only have 1 settings
+    async createUserSettings() {
+        let settings = await this.getUserSettings();
+        if (!settings) {
+            settings = await API.graphql(
+                graphqlOperation(createSetting, { input: { botMessage: " " } })
+            );
+        }
+    }
+
+    async updateUserSettings({ botMessage, hashtags }) {
+        const {
+            id,
+            botMessage: botMessageSettings,
+            hashtags: { items: hashtagSettings }
+        } = await this.getUserSettings();
+        const oldHashtags = hashtagSettings.map(({ name }) => name);
+        const arrayDifferences = symmetricDifference(hashtags, oldHashtags);
+        arrayDifferences.forEach(async hashtag => {
+            if (hashtags.includes(hashtag)) {
+                await API.graphql(
+                    graphqlOperation(createHashtag, {
+                        input: {
+                            name: hashtag,
+                            hashtagSettingId: this.settingsId
+                        }
+                    })
+                );
+                console.log(`Write ${hashtag}`);
+            } else {
+                const removeId = await this.getHashtagByName(hashtag);
+                await API.graphql(
+                    graphqlOperation(deleteHashtag, { input: { id: removeId } })
+                );
+                console.log(`Delete ${hashtag}`);
+            }
+        });
+        await API.graphql(
+            graphqlOperation(updateSetting, {
+                input: {
+                    id: this.settingsId,
+                    botMessage
+                }
+            })
+        );
+    }
+
+    onMessageChange = e => {
         this.setState({
-            botMessage:e.target.value
-        })
-    }
+            botMessage: e.target.value
+        });
+    };
 
-    onHashtagsChange = (e) => {
+    onHashtagsChange = e => {
         this.setState({
-            hashtags:e
-        })        
-    }
+            hashtags: e
+        });
+    };
 
-    onSave = (e) => {
-        e.preventDefault()
-        console.log("Submitted",this.state)
-    }
+    onSave = async e => {
+        e.preventDefault();
+        await this.updateUserSettings(this.state);
+    };
 
-    render(){
+    render() {
         return (
             <div
                 style={{
@@ -56,14 +136,23 @@ export default class Settings extends Component {
                         <InputLabel htmlFor="botMessage">
                             Bot Message
                         </InputLabel>
-                        <Input id="botMessage" type="text" onChange={this.onMessageChange} value={this.state.botMessage}/>
+                        <Input
+                            id="botMessage"
+                            type="text"
+                            onChange={this.onMessageChange}
+                            value={this.state.botMessage}
+                        />
                     </FormControl>
-                    <Button variant="contained" color="primary" size="medium" type="submit">
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        size="medium"
+                        type="submit"
+                    >
                         Save
                     </Button>
                 </form>
             </div>
-    );
-    } 
-    
+        );
+    }
 }
---
+++ src/utils/symmetricDifference.js
@@ -0,0 +1,5 @@
+export default (arr1, arr2) => {
+    return arr1
+        .filter(x => !arr2.includes(x))
+        .concat(arr2.filter(x => !arr1.includes(x)));
+}
