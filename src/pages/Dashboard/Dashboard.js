90dbfffb6223862dab731271cf310933b8b73700
hongzhi pan <victoryhongzhi@gmail.com>
2019-11-30 16:48:56 -0500

    convert functional component to class component for dashboard page

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import MainConsole from "../../components/MainConsole/MainConsole";
 import Map from "../../components/Map/Map";
 import MapControls from "../../components/MapControls/MapControls";
 import TagCloud from "../../components/TagCloud/TagCloud"
 import fakeReports from "../../utils/fakeMapReports";
 
-export default function Dashboard(props) {
-    return (
-        <>
-            <MainConsole horizontal_controls='top'>
+
+export class Dashboard extends Component {
+
+    state = {
+        reports: []
+    }
+
+    render() {
+        return (
+            <MainConsole horizontal_controls="top">
                 <Map
                     width={"100%"}
                     height={"100%"}
                     zoom={10}
                     latitude={40.73061}
                     longitude={-73.93524}
-                    issues={fakeReports()}
-                />
-                <MapControls />
-                <TagCloud />
+                    issues={this.state.reports}
+                >
+                
+                </Map>
+                <MapControls></MapControls>
+                <TagCloud></TagCloud>
             </MainConsole>
-        </>
-    );
+        )
+    }
 }
