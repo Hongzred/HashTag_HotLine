ca85642bd2dc0b7866e5ed166fbaf85cdd9664e2
hongzhi pan <victoryhongzhi@gmail.com>
2019-12-03 15:55:06 -0500

    use context api in map component

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -3,24 +3,27 @@ import MainConsole from '../../components/MainConsole/MainConsole'
 import Map from '../../components/Map/Map'
 import MapControls from '../../components/MapControls/MapControls'
 import TagCloud from '../../components/TagCloud/TagCloud'
-import fakeReports from '../../utils/fakeMapReports'
+// import fakeReports from '../../utils/fakeMapReports'
+import { UserStateContext } from '../../context/UserContext'
 
 export default function Dashboard() {
 	return (
-		<>
-			<MainConsole horizontal_controls="top">
-				<Map
-					width="100%"
-					height="100%"
-					zoom={10}
-					latitude={40.73061}
-					longitude={-73.93524}
-					issues={fakeReports()}
-				/>
-				<MapControls />
-				<TagCloud />
-			</MainConsole>
-		</>
+		<UserStateContext.Consumer>
+			{context => (
+				<MainConsole horizontal_controls="top">
+					<Map
+						width="100%"
+						height="100%"
+						zoom={10}
+						latitude={40.73061}
+						longitude={-73.93524}
+						issues={context.state.reports}
+					/>
+					<MapControls />
+					<TagCloud />
+				</MainConsole>
+			)}
+		</UserStateContext.Consumer>
 	)
 
 	// import React, { Component } from "react";
