0eaa11d72f973f96f64f061d919f5c543e2b274c
David Hadaller <dahadaller@gmail.com>
2019-12-07 14:57:13 -0500

    added response dialog component that allows call center worker to interact with user

--- src/components/TwitterFeed/Tweet.js
+++ src/components/TwitterFeed/Tweet.js
@@ -3,9 +3,9 @@ import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import ListItemAvatar from '@material-ui/core/ListItemAvatar'
 import Avatar from '@material-ui/core/Avatar'
-import ButtonGroup from '@material-ui/core/ButtonGroup'
-import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
+
+import ResponseDialog from '../ResponseDialog/ResponseDialog'
 
 export default function Tweet(props) {
 	const primary = `${props.full_name}  (${props.twitter_handle})`
@@ -24,15 +24,8 @@ export default function Tweet(props) {
 				</Grid>
 
 				<Grid item width="auto">
-					<ButtonGroup
-						size="small"
-						width="auto"
-						aria-label="full width outlined button group"
-					>
-						<Button>Resolved</Button>
-						<Button>Pending</Button>
-						<Button>Custom</Button>
-					</ButtonGroup>
+					{/* place the ResponseDialog Here */}
+					<ResponseDialog />
 				</Grid>
 			</Grid>
 		</ListItem>
