2d0e996849e78c12397928cf5b71051fe670e92b
Samuel Fils <smfils1@gmail.com>
2019-12-07 08:06:18 -0500

    Added createUserHAshtag test for hashName provided & for no settingId provided

--- src/__tests__/hashtags.test.js
+++ src/__tests__/hashtags.test.js
@@ -18,7 +18,16 @@ describe('createUserHashtag function', () => {
         expect(API.graphql).toHaveBeenCalledTimes(0)   
         expect(hashtag).toBe(undefined)     
     }) 
-
+    it('Create hashtag if there is hashtagName', async () => {
+		const hashtag = await createUserHashtag("name", "749b2b0c-6f78-4592-b427-e222e1e9855c")
+        expect(API.graphql).toHaveBeenCalledTimes(1) 
+        expect(hashtag).toBe("name")          
+    }) 
+    it('Create hashtag given an name & no settingsId', async () => {
+		const hashtag = await createUserHashtag("name")
+        expect(API.graphql).toHaveBeenCalledTimes(1) 
+        expect(hashtag).toBe("name")         
+    })
     
 })
 
