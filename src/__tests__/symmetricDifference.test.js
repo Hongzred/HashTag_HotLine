4dfad40026e2e02e8cb9fc0a35da6fdbb22d7f7e
Samuel Fils <smfils1@gmail.com>
2019-12-07 06:54:02 -0500

    Added symmetricDifference tests for non-empty array

--- src/__tests__/symmetricDifference.test.js
+++ src/__tests__/symmetricDifference.test.js
@@ -13,6 +13,32 @@ describe('The symmetric difference between 2 arrays', () => {
         const b = []
         const result = symmetricDifference(b,a)
         expect(result).toEqual(a)
-    })   
-  
+    })
+    
+    it('returns an empty array if both arrays are the same', () => {
+        const a = [1,2,3]
+        const b = [1,2,3]
+        const result = symmetricDifference(b,a)
+        expect(result).toEqual([])
+	})
+
+	it('returns elements in array1 that are not in array2 if array2 is a subset of array1.', async () => {
+        const array1 = [1,2,3]
+        let array2 = [2,3]
+        let result = symmetricDifference(array1,array2)
+        expect(result).toEqual([1])
+        array2 = [3]
+        result = symmetricDifference(array1,array2)
+        expect(result).toEqual([1,2])	
+    })
+    
+    it('returns elements in array2 that are not in array1 if array1 is a subset of array2.', async () => {
+        const array1 = [1,2]
+        let array2 = [1,2,3]
+        let result = symmetricDifference(array1,array2)
+        expect(result).toEqual([3])
+        array2 = [1]
+        result = symmetricDifference(array1,array2)
+        expect(result).toEqual([2])	
+	})
 })
