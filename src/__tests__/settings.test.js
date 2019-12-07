d89c846ae4c38c910c61923f294ef1703126b33a
Samuel Fils <smfils1@gmail.com>
2019-12-07 08:17:26 -0500

    Added getUserSettings test for when no settings exist

---
+++ src/__tests__/settings.test.js
@@ -0,0 +1,28 @@
+import { API } from 'aws-amplify'
+import { getUserSettings,createUserSettings,updateUserMessage } from '../crud/settings'
+import dbSettings from '../testData/dbSettings'
+
+
+afterEach(() => {
+	jest.clearAllMocks()
+})
+
+
+describe('getUserSettings function', () => {
+	it('Return no settings if it dont exist', async () => {
+		API.graphql.mockImplementationOnce(() =>
+			Promise.resolve({
+				data: {
+					listSettings: { items: [] },
+				},
+			}),
+		)
+		const settings = await getUserSettings()
+		expect(API.graphql).toHaveBeenCalledTimes(1)
+		expect(settings).toEqual(undefined)
+	})
+
+
+   
+    
+})
