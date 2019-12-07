1213b30902349e2bf1019ccf39f255b6d2150660
Samuel Fils <smfils1@gmail.com>
2019-12-07 08:20:33 -0500

    Added createUserSettings tests

--- src/__tests__/settings.test.js
+++ src/__tests__/settings.test.js
@@ -40,3 +40,34 @@ describe('getUserSettings function', () => {
     
     
 })
+
+describe('createUserSettings function', () => {
+	it('Dont create a new setting for user if one exist', async () => {
+		const settings = await createUserSettings(dbSettings.data.listSettings.items[0])
+		expect(API.graphql).toHaveBeenCalledTimes(0)
+        expect(settings).toEqual({
+            hashtags: ["mta_hth_test","accident_hth_test","mta_hth"],
+            botMessage: "We are looking into this issue",
+            settingsId: "749b2b0c-6f78-4592-b427-e222e1e9855c",
+        })
+    })
+    
+    it('Creates a new setting for user if one dont exist', async () => {
+        const settingsData = dbSettings.data.listSettings.items[0]
+        API.graphql.mockImplementationOnce(() =>
+        Promise.resolve({
+            data: {
+                createSetting: settingsData ,
+            },
+        }),
+    )
+    		const settings = await createUserSettings(undefined)
+		expect(API.graphql).toHaveBeenCalledTimes(1)
+        expect(settings).toEqual({
+            hashtags: ["mta_hth_test","accident_hth_test","mta_hth"],
+            botMessage: "We are looking into this issue",
+            settingsId: "749b2b0c-6f78-4592-b427-e222e1e9855c",
+        })
+	})   
+    
+})
