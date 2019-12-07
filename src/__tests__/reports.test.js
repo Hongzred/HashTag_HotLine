f322a35f744a42621acd57691fdab5debf212556
Samuel Fils <smfils1@gmail.com>
2019-12-07 08:25:14 -0500

    Added getUserReports test for when there's a  report

--- src/__tests__/reports.test.js
+++ src/__tests__/reports.test.js
@@ -22,4 +22,36 @@ describe('getUserReports function', () => {
 		expect(report).toEqual([])
 	})
 
+	it('Fetches a formated report', async () => {
+		API.graphql.mockImplementationOnce(() =>
+			Promise.resolve({
+				data: {
+					listReports: {
+						items: [dbReports.data.listReports.items[0]],
+					},
+				},
+			}),
+		)
+		const report = await getUserReports()
+
+		expect(API.graphql).toHaveBeenCalledTimes(1)
+		expect(report).toEqual([
+			{
+				date: 'Wed Dec 04 14:12:00 +0000 2019',
+				hashtags: ['accident_hth_test'],
+				id: '1e5de4aa-ab71-4a99-81bc-08f58ef3586f',
+				location: {
+					latitude: 40.656079999999996,
+					longitude: -73.915304,
+				},
+				post: '#accident_hth_test Bus accident.',
+				postId: '1202229065081610242',
+				spam: false,
+				status: 'PENDING',
+				userId: '1187559230414434304',
+				username: 'HashTagHotline',
+			},
+		])
+	})
+
 })
