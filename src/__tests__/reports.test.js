57f87f1cf936c9780fb257405684c5b922db64a9
Samuel Fils <smfils1@gmail.com>
2019-12-07 08:25:54 -0500

    Added getUserReports test for when there's multiple reports

--- src/__tests__/reports.test.js
+++ src/__tests__/reports.test.js
@@ -54,4 +54,42 @@ describe('getUserReports function', () => {
 		])
 	})
 
+	it('Fetches all reports formated', async () => {
+		API.graphql.mockImplementationOnce(() => Promise.resolve(dbReports))
+		const report = await getUserReports()
+
+		expect(API.graphql).toHaveBeenCalledTimes(1)
+		expect(report).toEqual([
+			{
+				date: 'Wed Dec 04 14:12:00 +0000 2019',
+				hashtags: ['accident_hth_test'],
+				id: '1e5de4aa-ab71-4a99-81bc-08f58ef3586f',
+				location: {
+					latitude: 40.656079999999996,
+					longitude: -73.915304,
+				},
+				post: '#accident_hth_test Bus accident.',
+				postId: '1202229065081610242',
+				spam: false,
+				status: 'PENDING',
+				userId: '1187559230414434304',
+				username: 'HashTagHotline',
+			},
+			{
+				date: 'Wed Dec 04 14:31:58 +0000 2019',
+				hashtags: ['accident_hth_test'],
+				id: 'e227c0a4-8bf1-4c91-8438-b51518ad886b',
+				location: {
+					latitude: 40.74195143555109,
+					longitude: -73.98936868406889,
+				},
+				post: '#accident_hth_test Tree fall..',
+				postId: '1202234090461810691',
+				spam: false,
+				status: 'PENDING',
+				userId: '1187559230414434304',
+				username: 'HashTagHotline',
+			},
+		])
+	})
 })
