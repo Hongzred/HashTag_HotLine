671e573c99b8fd1bb042387930d9782ea592703a
David Hadaller <dahadaller@gmail.com>
2019-12-07 19:38:53 -0500

    Now using the 'chip input' instead of a list of chips for the tag cloud component

--- src/components/TagCloud/SessionTagCloud.js
+++ src/components/TagCloud/SessionTagCloud.js
@@ -2,13 +2,17 @@ import React from 'react'
 import ChipInput from 'material-ui-chip-input'
 import { UserStateContext } from '../../context/UserContext'
 
+// TODO: Change defaultValue in the Chip Input to the session hashtags once
+// Sam is done with that
+
 export default function SessionTagCloud() {
 	return (
 		<UserStateContext.Consumer>
 			{context => (
 				<ChipInput
 					defaultValue={context.state.settings.hashtags}
-					label="Hashtags"
+					label="Temporary Session Hashtags"
+					placeholder="Type temporary filters here."
 					onChange={context.onHashtagsChange}
 					disableUnderline
 				/>
