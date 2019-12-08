20c6a4c46d5a91121fcc0e56eacc49558e3c0ff5
David Hadaller <dahadaller@gmail.com>
2019-12-07 21:44:32 -0500

    ResponseDialog now contains textarea where user can place a custom response

--- src/components/ResponseDialog/ResponseDialog.js
+++ src/components/ResponseDialog/ResponseDialog.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Dialog from '@material-ui/core/Dialog'
-import ListItemText from '@material-ui/core/ListItemText'
-import ListItem from '@material-ui/core/ListItem'
-import List from '@material-ui/core/List'
-import Divider from '@material-ui/core/Divider'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
@@ -13,6 +9,7 @@ import CloseIcon from '@material-ui/icons/Close'
 import Slide from '@material-ui/core/Slide'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
 import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 
 const useStyles = makeStyles(theme => ({
 	appBar: {
@@ -75,28 +72,23 @@ export default function ResponseDialog() {
 							<CloseIcon />
 						</IconButton>
 						<Typography variant="h6" className={classes.title}>
-							Sound
+							Response
 						</Typography>
 						<Button autoFocus color="inherit" onClick={handleClose}>
-							save
+							Send
 						</Button>
 					</Toolbar>
 				</AppBar>
-				<List>
-					<ListItem button>
-						<ListItemText
-							primary="Phone ringtone"
-							secondary="Titania"
-						/>
-					</ListItem>
-					<Divider />
-					<ListItem button>
-						<ListItemText
-							primary="Default notification ringtone"
-							secondary="Tethys"
-						/>
-					</ListItem>
-				</List>
+
+				<TextField
+					multiline
+					margin="normal"
+					style={{ margin: 30 }}
+					rows={10}
+					rowsMax={10}
+					variant="outlined"
+					label="Your Response"
+				/>
 			</Dialog>
 		</div>
 	)
