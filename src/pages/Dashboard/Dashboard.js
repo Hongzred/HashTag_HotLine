fbc4c8e8a78f844f1c2b74980f8d0bae9bd8a12d
Samuel Fils <smfils1@gmail.com>
2019-12-09 18:33:10 -0500

    Added Tag Component with disable state

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import Divider from '@material-ui/core/Divider'
 import MainConsole from '../../components/MainConsole/MainConsole'
 import Map from '../../components/Map/Map'
 import MapControls from '../../components/MapControls/MapControls'
-import TagCloud from '../../components/TagCloud/TagCloud'
+import Tag from '../../components/Tag/Tag'
 import SessionTagCloud from '../../components/TagCloud/SessionTagCloud'
 
 // import fakeReports from '../../utils/fakeMapReports'
@@ -25,8 +25,8 @@ export default function Dashboard() {
 					<MapControls />
 					<>
 						{/* <SessionTagCloud />
-						<Divider />
-						<TagCloud /> */}
+						<Divider />*/}
+						<Tag label="mta_hth" onDisable={context.onHashtagDisable} onEnable={context.onHashtagEnable}/> 
 					</>
 				</MainConsole>
 			)}
