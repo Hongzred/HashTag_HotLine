a6476f35e7e99660eccf5f89508a30c21f6db8ba
hongzhi pan <victoryhongzhi@gmail.com>
2019-12-11 00:25:12 -0500

    add more to user documenation

--- src/components/GettingStarted/GettingStarted.js
+++ src/components/GettingStarted/GettingStarted.js
@@ -9,9 +9,18 @@ const useStyles = makeStyles({
 	root: {
 		width: '100%',
 		maxWidth: 500,
+		overflow: 'auto',
 	},
 	li: {
 		margin: '10px 0',
+	},
+	image: {
+		width: '120px',
+		height: '120px',
+	},
+	image__tweet: {
+		width: '120px',
+		heigh: '200px',
 	},
 })
 export default function GettingStarted() {
@@ -29,21 +38,29 @@ export default function GettingStarted() {
 						<ul>
 							<li>enter the hashtags you want to search</li>
 							<li>customize the bot message</li>
+							<img
+								className={classes.image}
+								src="https://i.imgur.com/IHfmcN5.png"
+								alt="setting"
+							/>
 						</ul>
 					</li>
 					<li>View the results in the overview page</li>
-					<li>
-						Reply the tweet with actions
-						<ButtonGroup
-							size="small"
-							width="auto"
-							aria-label="full width outlined button group"
-						>
-							<Button>Resolved</Button>
-							<Button>Pending</Button>
-							<Button>Custom</Button>
-						</ButtonGroup>
-					</li>
+					<img
+						className={classes.image__tweet}
+						src="https://i.imgur.com/4WFAj0Z.png"
+						alt="tweet"
+					/>
+					<li>Reply the tweet with actions</li>
+					<ButtonGroup
+						size="small"
+						width="auto"
+						aria-label="full width outlined button group"
+					>
+						<Button>Resolved</Button>
+						<Button>Pending</Button>
+						<Button>Custom</Button>
+					</ButtonGroup>
 					<li>
 						you can also filter the results by distances and time!
 					</li>
